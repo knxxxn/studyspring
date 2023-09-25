2cbc9d585b313511b06ed1a8dd59bbea36cb6d22
knxxxn <20211308@sungshin.ac.kr>
2023-09-25 14:49:47 +0900

    Initial commit

---
+++ src/main/java/studyspring/studyspring/StudyspringApplication.java
@@ -0,0 +1,14 @@
+package studyspring.studyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyspringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(StudyspringApplication.class, args);
+	}
+
+}
