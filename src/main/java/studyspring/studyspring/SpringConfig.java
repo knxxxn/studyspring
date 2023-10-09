bcad0fc267ef791306430cc48f9eb328bf408a60
knxxxn <20211308@sungshin.ac.kr>
2023-10-09 15:02:54 +0900

    섹션 6-3까지의 코드 - 스프링DB접근 기술 -1

--- src/main/java/studyspring/studyspring/SpringConfig.java
+++ src/main/java/studyspring/studyspring/SpringConfig.java
@@ -2,12 +2,20 @@ package studyspring.studyspring;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import studyspring.studyspring.repository.JdbcMemberRepository;
 import studyspring.studyspring.repository.MemberRepository;
 import studyspring.studyspring.repository.MemoryMemberRepository;
 import studyspring.studyspring.service.MemberService;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository()); //memberRepository 사용
@@ -15,6 +23,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+
+        return new JdbcMemberRepository(dataSource);
     }
 }
